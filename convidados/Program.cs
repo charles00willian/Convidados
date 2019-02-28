5f046d2a0097479f017c44d48f021f6c33ab51ef
Charles Willian <wcharlesw15@gmail.com>
2019-02-28 19:12:16 -0300

    Initial commit

---
+++ convidados/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convidados
+{
+    class Program
+    {
+        static int menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Exibir lista");
+            Console.WriteLine("2 - Inserir convidado");
+            Console.WriteLine("3 - Remover convidado");
+            Console.WriteLine("4 - Consultar convidado");
+            Console.WriteLine("5 - Consultar velhos");
+            Console.WriteLine("6 - Remover jovens");
+            Console.WriteLine("15 - Sair");
+            Console.Write("Opção: ");
+
+            return (int.Parse(Console.ReadLine()));
+        }
+
+        static void Main(string[] args)
+        {
+            int op;
+
+            do
+            {
+                op = menu();
+
+                switch (op)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+                }
+
+            } while (op != 15);
+        }
+    }
+}
